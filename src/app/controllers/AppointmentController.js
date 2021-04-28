822b0a6a953ece9a3edbaced74d42d7efe08208a
unknown <davisilcampos@hotmail.com>
2021-04-28 20:57:25 -0300

    Validation of Appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-
+import {startOfHour, parseISO, isBefore} from 'date-fns'
 import User from './../models/User'
 import Appointment from './../models/Appointment'
 
@@ -25,7 +25,27 @@ class AppointmentControoler {
         });
 
         if (!isProvider){
-            return res.status(401).json({ error: "Acesso restrito à prestadores"})
+            return res.status(401).json({ error: "Acesso restrito"})
+        }
+
+        const hourStart = startOfHour(parseISO(date));
+
+        //checando se a data é no passado
+        if (isBefore(hourStart, new Date())){
+            return res.status(400).json({ error: "Data inválida para agendamento!"})
+        }
+
+        //chegado se tem disponibilidade pra agendamento
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id: provider_id,
+                canceled_at: null,
+                date: hourStart,
+            }
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({ error: "Sem vagas para essa data, marque outra"})
         }
 
         const appointment = await Appointment.create({
